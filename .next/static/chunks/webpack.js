699fe830c9910e7aee0e81626837bc55fac2bd04
devramirez <aramirezdev1@gmail.com>
2023-10-13 00:14:56 -0400

    fixed bug under header section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6906f34b8122a957"; }
+/******/ 		__webpack_require__.h = function() { return "600d13f7642f83d0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
